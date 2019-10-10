dfcbcefeb83523fa4aedf9d50059959970f8321a
nanhanglim-dev <56348371+nanhanglim-dev@users.noreply.github.com>
2019-10-10 19:29:24 +0800

    Update shop.js

--- iki4you/js/shop.js
+++ iki4you/js/shop.js
@@ -7,7 +7,7 @@ function getCurateParameter() {
 function getPMU() {
   if (document.getElementById("pmuPetite").checked == false && document.getElementById("pmuRegular").checked == false && document.getElementById("pmuDeluxe").checked == false) {
     document.getElementById('boxSelectText').style.display = "block";
-  } else if (document.getElementById("pmuPetite").checked == true;) {
+  } else if (document.getElementById("pmuPetite").checked == true) {
     var pickMeUp = "petite"
     location.replace("/curate-3");
   } else if (document.getElementById("pmuRegular").checked == true) {
@@ -30,7 +30,7 @@ function getBYC() {
   var bycDDate = document.getElementById("bycDDate").value;
   if (document.getElementById("byc10AM6PM").checked == false && document.getElementById("byc6PM10PM").checked == false) {
     document.getElementById('timeSelectText').style.display = "block";
-  } else if (document.getElementById("pmu10AM6PM").checked == true;) {
+  } else if (document.getElementById("pmu10AM6PM").checked == true) {
     var bycTime = "10AM-6PM"
   } else if (document.getElementById("pmu6PM10PM").checked == true) {
     var bycTime = "6PM-10PM"
@@ -40,25 +40,25 @@ function getBYC() {
 function getAFT() {
   if (document.getElementById("aftLIS").checked == false && document.getElementById("aftHALD").checked == false && document.getElementById("aftHV").checked == false && document.getElementById("aftTAARTS").checked == false && document.getElementById("aftIYB").checked == false && document.getElementById("aftBon").checked == false && document.getElementById("aftTY").checked == false) {
     document.getElementById('msgSelectText').style.display = "block";
-  } else if (document.getElementById("aftLIS").checked == true;) {
+  } else if (document.getElementById("aftLIS").checked == true) {
     var aftDesign = "Let it sparkle"
   } else if (document.getElementById("aftHALD").checked == true) {
     var aftDesign = "Have a lovely day"
   } else if (document.getElementById("aftHV").checked == true) {
     var aftDesign = "Happy vibes"
-  } else if (document.getElementById("aftTARRTS").checked == true;) {
+  } else if (document.getElementById("aftTARRTS").checked == true) {
     var aftDesign = "There\'s always a reason to smile"
-  } else if (document.getElementById("aftIYB").checked == true;) {
+  } else if (document.getElementById("aftIYB").checked == true) {
     var aftDesign = "It\'s your birthday"
-  } else if (document.getElementById("aftBon").checked == true;) {
+  } else if (document.getElementById("aftBon").checked == true) {
     var aftDesign = "Bonjour"
-  } else if (document.getElementById("aftTY").checked == true;) {
+  } else if (document.getElementById("aftTY").checked == true) {
     var aftDesign = "Thank you"
   }
   var aftMessage = document.getElementById("aftMessage").value
   if (document.getElementById("aftRustic").checked == false && document.getElementById("aftClassic").checked == false) {
     document.getElementById('timeSelectText').style.display = "block";
-  } else if (document.getElementById("aftRustic").checked == true;) {
+  } else if (document.getElementById("aftRustic").checked == true) {
     var aftLook = "Rustic"
   } else if (document.getElementById("aftClassic").checked == true) {
     var aftLook = "Classic"
