5aa8ab4028f726df132b872dc0ea3244fb0c6173
nanhanglim-dev <56348371+nanhanglim-dev@users.noreply.github.com>
2019-10-11 19:40:13 +0800

    Update shop.js

--- iki4you/js/shop.js
+++ iki4you/js/shop.js
@@ -1,4 +1,4 @@
-/* project i v1.0.2 | (c) 2019 Nan Hang Lim (@nanhanglim)  */
+/* project i v1.0.3 | (c) 2019 Nan Hang Lim (@nanhanglim)  */
 
 function getCurateParameter() {
   var params = new URLSearchParams(document.location.search.substring(1));
